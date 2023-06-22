c487c6cd217304522868b161f25c4028d434e83e
Christine Maria Metz <Christine.maria.metz@gmail.com>
2023-06-22 10:18:01 +0200

    Done: Task 03

--- 03-attribute-binding/script.js
+++ 03-attribute-binding/script.js
@@ -5,7 +5,7 @@ Vue.createApp({
     return {
       headline: "Attribute Binding is awesome",
       imageAttrs: {
-        src: "https://picsum.photos/id/237/200/300",
+        src: "https://picsum.photos/id/237/500/500",
         alt: "A cute dog",
       },
     };

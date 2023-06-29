665f56a1bf434af13658afa2ff11cd47b1f1baaf
Christine Maria Metz <Christine.maria.metz@gmail.com>
2023-06-29 16:09:53 +0200

    fix: bug fix in css file

--- VueCLI-projects/book-store/src/main.js
+++ VueCLI-projects/book-store/src/main.js
@@ -2,6 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "@/assets/reset.css";
-import "@assets/global.css";
+import "@/assets/global.css";
 
 createApp(App).mount("#app");
